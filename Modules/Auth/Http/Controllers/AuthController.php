32c7281ec6994e6c95a0c6e3c5690efd7684c54e
mhannan <mdhannan.info@gmail.com>
2022-07-30 15:38:06 +0600

    ADD AUTH MODULE

---
+++ Modules/Auth/Http/Controllers/AuthController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Modules\Auth\Http\Controllers;
+use Illuminate\Routing\Controller;
+use Illuminate\Contracts\Support\Renderable;
+
+
+class AuthController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     * @return Renderable
+     */
+    public function index()
+    {
+        dd("oaky");
+        
+    }
+}
